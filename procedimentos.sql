07fb73a931a7336492d1cd9d60cc496f9e0020dc
SantanaLari <88253169+SantanaLari@users.noreply.github.com>
2022-11-13 16:44:31 -0300

    Alteração de function e adição de outra

--- procedimentos.sql
+++ procedimentos.sql
@@ -63,15 +63,15 @@ AS
 	END
 
 ---------------------------------------------------- FUNCTION NOTAS [incompleto] ----------------------------------------------
-ALTER FUNCTION fn_notas(@cod VARCHAR(10))
+ALTER FUNCTION fn_notas(@codDisciplina VARCHAR(10))
 RETURNS @tabela TABLE(
 ra_aluno INT,
---nome_aluno VARCHAR(20),
+nome_aluno VARCHAR(20),
 nota1 DECIMAL(7,2), --P1
 nota2 DECIMAL(7,2), --P2
-nota3 DECIMAL(7,2), --Trabalho / P3 (LBD)
-nota4 DECIMAL(7,2), --PRE P3 (SOI) 
-P3 DECIMAL(7,2), --EXAME
+nota3 DECIMAL(7,2), --Trabalho / P3(LBD)
+nota4 DECIMAL(7,2), --PRE P3 (SOI)
+exame DECIMAL(7,2), --EXAME
 media_final DECIMAL(7,2),
 situacao VARCHAR(20)
 )
@@ -83,170 +83,182 @@ BEGIN
 			@nome VARCHAR(20),
 			@media DECIMAL(7,2),
 			@situacao VARCHAR(20),
-			@P3 DECIMAL(7,2)
-
+			@P3 DECIMAL(7,2),
+			@tipo INT,
+			@exame DECIMAL(7,2)
 	SET @media = 0
 	SET @P3 = 0
 
+
 	DECLARE c CURSOR FOR
-		SELECT DISTINCT ra_aluno FROM notas WHERE codigo_disciplina = @cod
+		SELECT DISTINCT ra_aluno FROM notas WHERE codigo_disciplina = @codDisciplina
 	OPEN c
 	FETCH NEXT FROM c INTO @ra
 	WHILE @@FETCH_STATUS = 0
 	BEGIN
-		INSERT INTO @tabela VALUES(@ra, 0,0,0,0,0,0, NULL)
-	-------------------------------------------------------------------------
+		SET @nome = (SELECT nome from aluno WHERE ra = @ra)
+		INSERT INTO @tabela VALUES(@ra, @nome, 0, 0, 0, 0, 0, 0, NULL)
+		
+	--------------------------------------------------------------------------------------
 		DECLARE c1 CURSOR FOR
-			SELECT codigo_avaliacao, nota FROM notas WHERE codigo_disciplina = @cod AND ra_aluno = @ra
+			SELECT codigo_avaliacao, nota FROM notas WHERE codigo_disciplina = @codDisciplina AND ra_aluno = @ra
 		OPEN c1
 		FETCH NEXT FROM c1 INTO @avaliacao, @nota
 		WHILE @@FETCH_STATUS = 0
 		BEGIN
-		------------------------------ AOC / LABH / BD -----------------------
-		IF ((@cod = '4203-010') OR (@cod = '4203-020') OR (@cod = '4208-010') OR (@cod = '4226-004')) 
+
+		IF((@codDisciplina = '4203-010') OR (@codDisciplina = '4203-020') OR (@codDisciplina = '4208-010') OR (@codDisciplina = '4226-004')) 
 		BEGIN
-			IF(@avaliacao = 1) --P1
-			BEGIN
-				UPDATE @tabela
-				SET nota1 = @nota, media_final = (@nota * 0.3) + media_final
-				WHERE ra_aluno = @ra
-			END
-			ELSE
-			IF (@avaliacao = 2) --P2
-			BEGIN
-				UPDATE @tabela
-				SET nota2 = @nota, media_final = (@nota * 0.5) + media_final
-				WHERE ra_aluno = @ra
-			END
-			ELSE
-			IF (@avaliacao = 6) -- TRABALHO
-			BEGIN
-				UPDATE @tabela
-				SET nota3 = @nota, media_final = (@nota * 0.2) + media_final
-				WHERE ra_aluno = @ra
-			END
-			IF (@avaliacao = 3) -- P3
-			BEGIN
-				UPDATE @tabela
-				SET P3 = @nota
-				WHERE ra_aluno = @ra
-				SET @P3 = @nota
-			END
+			SET @tipo = 1
+		END
+		ELSE 
+		IF ((@codDisciplina = '4213-003') OR (@codDisciplina = '4213-013'))
+		BEGIN
+			SET @tipo = 2
+		END
+		IF(@codDisciplina = '4233-005')
+		BEGIN
+			SET @tipo = 3
+		END
+		ELSE
+		IF(@codDisciplina = '5005-220')
+		BEGIN
+			SET @tipo = 4
 		END
 		
-		------------------------------ SOI -----------------------
-		IF((@cod = '4213-003') OR (@cod = '4213-013'))
+		IF (@avaliacao = 1) -- P1
 		BEGIN
-			IF(@avaliacao = 1) --P1
-			BEGIN
-				UPDATE @tabela
-				SET nota1 = @nota, media_final = (@nota * 0.35) + media_final
-				WHERE ra_aluno = @ra
-			END
-			IF(@avaliacao = 2) --P2
-			BEGIN
-				UPDATE @tabela
-				SET nota2 = @nota, media_final = (@nota * 0.35) + media_final
-				WHERE ra_aluno = @ra
-			END
-			IF(@avaliacao = 6) --T
-			BEGIN
-				UPDATE @tabela
-				SET nota3 = @nota, media_final = (@nota * 0.3) + media_final
-				WHERE ra_aluno = @ra
-			END
-			IF(@avaliacao = 5) --Pre P3
-			BEGIN
-				UPDATE @tabela
-				SET nota4 = @nota, media_final = (@nota * 0.2) + media_final
-				WHERE ra_aluno = @ra
-			END
-			IF(@avaliacao = 3) --P3
-			BEGIN
-				UPDATE @tabela
-				SET P3 = @nota
-				WHERE ra_aluno = @ra
-				SET @P3 = @nota
-			END
+			UPDATE @tabela
+			SET nota1 = @nota, media_final = (SELECT dbo.calculaNota(@avaliacao, @nota, @tipo)) + media_final
+			WHERE ra_aluno = @ra
 		END
-
-		------------------------------ LBD -----------------------
-		IF(@cod = '4233-005')
+		IF (@avaliacao = 2) -- P2
 		BEGIN
-			IF(@avaliacao = 1) --P1
-			BEGIN
-				UPDATE @tabela
-				SET nota1 = @nota, media_final = (@nota * 0.333) + media_final
-				WHERE ra_aluno = @ra
-			END
-			IF(@avaliacao = 2) --P2
-			BEGIN
-				UPDATE @tabela
-				SET nota2 = @nota, media_final = (@nota * 0.333) + media_final
-				WHERE ra_aluno = @ra
-			END
-			IF(@avaliacao = 3) --P3
-			BEGIN
-				UPDATE @tabela
-				SET nota3 = @nota, media_final = (@nota * 0.333) + media_final
-				WHERE ra_aluno = @ra
-			END
-			IF(@avaliacao = 4) --P4
-			BEGIN
-				UPDATE @tabela
-				SET P3 = @nota
-				WHERE ra_aluno = @ra
-				SET @P3 = @nota
-			END
+			UPDATE @tabela
+			SET nota2 = @nota, media_final = (SELECT dbo.calculaNota(@avaliacao, @nota, @tipo)) + media_final
+			WHERE ra_aluno = @ra
 		END
-
-		------------------------------ MPC -----------------------
-		IF(@cod = '5005-220')
+		IF (@avaliacao = 6) -- TRABALHO
 		BEGIN
-			IF(@avaliacao = 1) --P1
-			BEGIN
-				UPDATE @tabela
-				SET nota1 = @nota, media_final = (@nota * 0.8) + media_final
-				WHERE ra_aluno = @ra
-			END
-			IF(@avaliacao = 2) --P2
-			BEGIN
-				UPDATE @tabela
-				SET nota2 = @nota, media_final = (@nota * 0.2) + media_final
-				WHERE ra_aluno = @ra
-			END
+			UPDATE @tabela
+			SET nota3 = @nota, media_final = (SELECT dbo.calculaNota(@avaliacao, @nota, @tipo)) + media_final
+			WHERE ra_aluno = @ra
 		END
-
+		-------------------------------------------EXCEÇÕES
+		--P3 DE LBD
+		IF (@avaliacao = 3) AND (@tipo = 3)
+		BEGIN
+			UPDATE @tabela
+			SET nota3 = @nota, media_final = (@nota * 0.333) + media_final
+			WHERE ra_aluno = @ra
+		END
+		--PRE P3 DE SOI
+		IF(@avaliacao = 5) AND (@tipo = 2)
+		BEGIN
+			UPDATE @tabela
+			SET nota4 = @nota, media_final = (@nota * 0.2) + media_final
+			WHERE ra_aluno = @ra
+		END
+		
 		FETCH NEXT FROM c1 INTO @avaliacao, @nota
 		END
 		CLOSE c1
 		DEALLOCATE c1
-	-------------------------------------------------------------------------
+	--------------------------------------------------------------------------------------
 		SET @media = (SELECT media_final FROM @tabela WHERE ra_aluno = @ra)
-		IF(@media < 6)
-		BEGIN
-			IF((@media > 3) AND (@P3 = 0))
-			BEGIN
-				UPDATE @tabela 
-				SET situacao = 'Exame'
-			END
-			ELSE
-			BEGIN
-				UPDATE @tabela
-				SET situacao = 'Reprovado'
-			END
-		END
-		ELSE
+		IF(@media >= 6)
 		BEGIN
 			UPDATE @tabela
 			SET situacao = 'Aprovado'
+			WHERE ra_aluno = @ra
 		END
-
+		ELSE
+		IF (@media < 6)
+		BEGIN
+			UPDATE @tabela
+			SET situacao = 'Analise'
+			WHERE ra_aluno = @ra
+		END
+	
 	FETCH NEXT FROM c INTO @ra
 	END
 	CLOSE c
 	DEALLOCATE c
 	RETURN
+END
+---------------------------------------------- FUNCTION QUE CALCULA NOTA
+ALTER FUNCTION calculaNota(@avaliacao INT, @nota DECIMAL(7,2), @tipo INT)
+RETURNS DECIMAL(7,2)
+AS
+BEGIN
+	DECLARE @calcNota DECIMAL(7,2)
+	
+------------------------------------- P1
+	IF(@avaliacao = 1)
+	BEGIN
+		IF(@tipo = 1)
+		BEGIN
+			SET @calcNota = @nota * 0.3
+		END
+		ELSE 
+		IF (@tipo = 2)
+		BEGIN 
+			SET @calcNota = @nota * 0.35
+		END
+		ELSE
+		IF (@tipo = 3)
+		BEGIN
+			SET @calcNota = @nota * 0.333
+		END
+		ELSE
+		IF(@tipo = 4)
+		BEGIN
+			SET @calcNota = @nota * 0.8
+		END
+	END
+
+------------------------------------- P2
+	IF(@avaliacao = 2)
+	BEGIN
+		IF(@tipo = 1)
+		BEGIN
+			SET @calcNota = @nota * 0.5
+		END
+		ELSE 
+		IF (@tipo = 2)
+		BEGIN 
+			SET @calcNota = @nota * 0.35
+		END
+		ELSE
+		IF (@tipo = 3)
+		BEGIN
+			SET @calcNota = @nota * 0.333
+		END
+		ELSE
+		IF(@tipo = 4)
+		BEGIN
+			SET @calcNota = @nota * 0.2
+		END
+	END
+
+------------------------------------- TRABALHO
+	IF(@avaliacao = 6)
+	BEGIN
+		IF(@tipo = 1)
+		BEGIN
+			SET @calcNota = @nota * 0.2
+		END
+		ELSE 
+		IF (@tipo = 2)
+		BEGIN 
+			SET @calcNota = @nota * 0.3
+		END
+	END
+
+------------------------------------- EXAME = P3 (Todos, menos LBD), P4 (apenas LBD)
+--ESSA PARTE AINDA NÃO FOI FEITA
+
+	RETURN(@calcNota)
 END
 
 
