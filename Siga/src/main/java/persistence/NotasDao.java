07093d62d3e1f2178c32848f6cbfc50ed0171925
SantanaLari <88253169+SantanaLari@users.noreply.github.com>
2022-11-19 17:28:01 -0300

    alteração no metodo inserirNotas

--- Siga/src/main/java/persistence/NotasDao.java
+++ Siga/src/main/java/persistence/NotasDao.java
@@ -20,8 +20,8 @@ public class NotasDao implements INotasDao {
 		String sql = "EXEC pc_insereNota @ra = ?, @disciplina = ?, @avaliacao = ?, @nota = ? ";
 		PreparedStatement ps = c.prepareStatement(sql);
 		ps.setInt(1, n.getAluno().getRa());
-		ps.setString(2, n.getDisciplina().getSigla());
-		ps.setString(3, n.getAvaliacao().getTipo());
+		ps.setString(2, n.getDisciplina().getCodigo());
+		ps.setInt(3, n.getAvaliacao().getCodigo());
 		ps.setDouble(4, n.getNota());
 		ps.execute();
 		ps.close();
